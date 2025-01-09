ce2db4d06b01f963f13109855e66c80f1c3c5848
Baachi13 <jeremiasavaro7@gmail.com>
2025-01-09 11:25:23 -0300

    First scaffold created, and bootstrap added in the app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Define the root (home page) route to be the posts index page
+  root "posts#index"
+end
